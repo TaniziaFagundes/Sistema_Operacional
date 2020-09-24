f619715b1cd8a21cfae3c2766473419ddbf5d57b
Tanizia Fagundes <tdlf@icomp.ufam.edu.br>
2020-09-24 18:59:35 -0400

    implementacao ls

--- pronto_terminal/terminal.c
+++ pronto_terminal/terminal.c
@@ -6,18 +6,15 @@
 
 int main(int argc, char *argv[]){
     printf("hello world (pid:%d)\n", (int) getpid());
+
     int i=0;
     char sFrase[50];
     char *palavra[5];
     scanf("%[^\n]",sFrase);  
 
     char *psRetorno;
-    
-
-   // Primeiro busca por espaço em branco.
     psRetorno = strtok(sFrase, " "); //pega a primeira palavra antes do espaço
 
-    //printf("\npsRetorno = %s", psRetorno);
 
     while (psRetorno != NULL)
     {
@@ -25,21 +22,43 @@ int main(int argc, char *argv[]){
         psRetorno = strtok(NULL," ");  //faz todos os espaços serem nulos
     }palavra[i] = NULL; //insere NULL no ultimo elemento do vetor;
 
-    /* TESTE
-    printf("\nFrase = %s", sFrase);
-    printf("\nPalavra = %s", palavra[0]);
-    printf("\nPalavra = %s", palavra[1]);
-    printf("\nPalavra = %s", palavra[2]);
-    */
+
     
-    int rc = fork();
-    if (rc < 0) {// fork falhou
-        fprintf(stderr, "fork falhou\n");
-        exit(1);
-    } else if (rc == 0) { // filho
-       
-    } else{// Pai vem por aqui
+    if(palavra[1] == "|"){
+        // faz a saída de prog1 ser a entrada de prog2
+    }else if(palavra[1] == "&"){
+       // roda prog1 e deixa o terminal livre para outros comandos. ?
+    }else if(palavra[1] == ">"){
+        // saida - roda prog1 e joga o output dele para o arquivo saída
+    }else if(palavra[1] == "<"){
+       // entrada – roda prog1 e recebe como input o arquivo entrada
+    }else{
+        //se não for nenhuma desses significa que são comandos do tipo cd nomepasta, ls, pwd, etc..
         
+        int rc = fork();  
+        if (rc < 0) {      // fork falhou
+            fprintf(stderr, "fork falhou\n");
+            exit(1);
+        }else if (rc == 0) { // filho - todos os recursos do processo filho aqui
+            if(strcmp(palavra[0],"ls") == 0){
+                execvp(strdup(palavra[0]),palavra);
+            }
+
+        }else{// Pai vem por aqui
+            int wc = wait(NULL);
+            printf("\n hello, sou o pai de %d (wc:%d) (pid:%d)\n",
+            rc, wc, (int) getpid());
+
+        }
+        return 0;
     }
-    return 0;
 }
+
+/* pedaço do codigo do professor para consulta
+
+        myargs[0] = strdup("ls");   // programa: "wc"
+        myargs[1] = strdup("-l");   // argumento: nome do arquivo
+        myargs[2] = NULL;           // fim do vetor
+        execvp(myargs[0], myargs);  // roda wc
+
+*/
